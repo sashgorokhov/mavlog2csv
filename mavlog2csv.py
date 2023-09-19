003a315a60ed3fdfc32727aa16e485a91d341392
Alexander Gorokhov <sashgorokhov@gmail.com>
2023-09-19 15:51:13 -0500

    Initial code push

---
+++ mavlog2csv.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+"""
+Used https://github.com/ArduPilot/pymavlink/blob/master/tools/mavlogdump.py for reference.
+"""
+import argparse
+import collections
+import contextlib
+import csv
+import datetime
+import logging
+import operator
+import re
+import sys
+import textwrap
+from typing import (
+    IO,
+    Any,
+    ContextManager,
+    Dict,
+    Iterator,
+    List,
+    Optional,
+    Set,
+    Tuple,
+    Union,
+)
+
+from pymavlink import mavutil
+from pymavlink.CSVReader import CSVReader
+from pymavlink.DFReader import DFReader
+from pymavlink.dialects.v10.ardupilotmega import MAVLink_message
+from pymavlink.mavutil import mavserial
+
+logger = logging.getLogger(__name__)
+
+
+def is_message_bad(message: Optional[MAVLink_message]) -> bool:
+    """Check if message is bad and work working with"""
+    return bool(message is None or (message and message.get_type() == "BAD_DATA"))
+
+
+@contextlib.contextmanager
+def mavlink_connect(device: str) -> Union[mavserial, DFReader, CSVReader]:
+    """
+    Create mavlink connection to a device. Device can be anything from comport, to .bin or .log ardupilot telemetry log file.
+    Will close the connection once context is exited
+    """
+    conn = mavutil.mavlink_connection(device)
+    logger.debug(f"Connecting to {device}")
+    yield conn
+    logger.debug(f"Closing connection to {device}")
+    conn.close()
+
+
+def parse_cli_column(cli_col: str) -> Tuple[str, str]:
+    """
+    Parse CLI provided column into message type and column name parts.
+    """
+    match = re.match("(?P<message_type>\w+)\.(?P<column>\w+)", cli_col)
+    if not match:
+        raise ValueError(
+            f"""\
+            Specified column is not correct format:
+            Column "{cli_col}" must be <Message type>.<Column>.
+            For example: GPS.Lat
+        """
+        )
+    return match.group(1), match.group(2)
+
+
+def open_output(output: Optional[str] = None) -> ContextManager[IO]:
+    """
+    Either opens a file `output` for writing or returns STDOUT stream"""
+    if output:
+        return open(output, "w", newline="")
+    else:
+        return contextlib.nullcontext(sys.stdout)
+
+
+def iter_mavlink_messages(
+    device: str, types: Set[str], skip_n_arms: int = 0
+) -> Iterator[MAVLink_message]:
+    """
+    Return iterator over mavlink messages of `types` from `device`.
+    If skip_n_arms is not zero, will return messages only after skip_n_arms ARM messages has been seen.
+    """
+    types = types.copy()
+    types.add("EV")  # EV are events like ARM (id=10) or DISARM (id=11)
+    n_message = 0
+    n_armed = 0
+    with mavlink_connect(device) as mav_conn:
+        while True:
+            message: Optional[MAVLink_message] = mav_conn.recv_match(
+                blocking=False, type=types
+            )
+            n_message += 1
+
+            if message is None:
+                logger.debug(f"Stopping processing at {n_message} message")
+                break
+
+            if is_message_bad(message):
+                continue
+
+            if message.get_type() == "EV" and message.Id == 10:  # arm
+                logger.debug(f"Found ARM event: {message}")
+                n_armed += 1
+
+            if n_armed < skip_n_arms or message.get_type() == "EV":
+                continue
+
+            yield message
+
+
+def message_to_row(message: MAVLink_message, columns: List[str]) -> Dict[str, Any]:
+    """Convert mavlink message to output row"""
+    row: Dict[str, Any] = {}
+
+    row["TimeUS"] = message.TimeUS
+    row["TimeS"] = round(message.TimeUS / 1_000_000, 2)
+    dt = datetime.datetime.fromtimestamp(message._timestamp)
+    row["Date"] = dt.date().isoformat()
+    row["Time"] = dt.time().isoformat()
+
+    for col in columns:
+        col_value = getattr(message, col, None) or ""  # Place for improvement
+        row[f"{message.get_type()}.{col}"] = col_value
+
+    return row
+
+
+def mavlog2csv(
+    device: str, columns: List[str], output: Optional[str] = None, skip_n_arms: int = 0
+):
+    """
+    Convert ardupilot telemetry log into csv with selected columns.
+    Specify the input file (.bin telemetry log), some desired telemetry columns (like GPS.Lat), and observe the magic.
+    You can find message types and their column reference here: https://ardupilot.org/copter/docs/logmessages.html.
+    """
+    parsed_columns: List[Tuple[str, str]] = list(map(parse_cli_column, columns))
+
+    # Collects all required message types like {'GPS', 'ATT', 'ASPD'}
+    # Used to filter mavlink messages
+    message_type_filter: Set[str] = set(map(operator.itemgetter(0), parsed_columns))
+
+    # Collects a mapping message type -> columns
+    # Used to quickly extract required columns from message
+    message_type_columns: Dict[str, List[str]] = collections.defaultdict(list)
+    for message_type, column in parsed_columns:
+        message_type_columns[message_type].append(column)
+
+    header = [
+        "TimeUS",  # Original time in US after boot
+        "TimeS",  # Seconds after boot
+        "Date",  # Calculated date of the event
+        "Time",  # Calculated time of the event,
+        *columns,  # User specified columns
+    ]
+
+    with open_output(output) as output_file:
+        csv_writer = csv.DictWriter(
+            output_file,
+            fieldnames=header,
+            delimiter=",",
+            quotechar='"',
+            quoting=csv.QUOTE_ALL,
+        )
+        csv_writer.writeheader()
+        for message in iter_mavlink_messages(
+            device=device, types=message_type_filter, skip_n_arms=skip_n_arms
+        ):
+            message_type = message.get_type()
+            if message_type in message_type_columns:
+                row = message_to_row(message, message_type_columns[message_type])
+                csv_writer.writerow(row)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description=textwrap.dedent(mavlog2csv.__doc__),  # type: ignore
+        epilog=textwrap.dedent(
+            """\
+            Example usage:
+
+            # Output GPS Longitude and latitude and airspeed sensor readings
+            python mavlog2csv.py -c GPS.Lng -c GPS.Lat -c ARSP.Airspeed -o output.csv "2023-09-17 13-34-16.bin"
+
+            # Redirecting stdout into a file on windows. Not recommended, use -o instead.
+            python mavlog2csv.py -c GPS.Lng "2023-09-17 13-34-16.bin" 1> output.csv
+        """
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument("input", help="Input file name.")
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Output file name. If not set, script will output into stdout.",
+    )
+    parser.add_argument(
+        "-c",
+        "--col",
+        action="append",
+        required=True,
+        help="Specify telemetry columns to output. Format: <Message type>.<Column>. For example: GPS.Lng",
+    )
+    parser.add_argument(
+        "--skip-n-arms",
+        type=int,
+        default=0,
+        help="If there are multiple arm events in the log, skip this number of arms before writing any rows at all. "
+        "If you setup to log only after autopilot was armed, then first arm event wont be stored in the log.",
+    )
+
+    args = parser.parse_args()
+
+    mavlog2csv(
+        device=args.input,
+        columns=args.col,
+        skip_n_arms=args.skip_n_arms,
+        output=args.output,
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+    main()
---
+++ tests/test_mavlog2csv.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+import contextlib
+import csv
+import datetime
+import inspect
+from io import IOBase
+from typing import List, Optional
+from unittest import mock
+
+import pytest
+from pymavlink.DFReader import DFReader
+from pymavlink.dialects.v10.ardupilotmega import MAVLink_message
+
+import mavlog2csv
+
+
+class StubMavlink:
+    def __init__(self):
+        self.messages: List[MAVLink_message] = []
+
+    def start(self):
+        self.msg_queue = iter(self.messages)
+
+    def recv_match(self, blocking, type) -> Optional[MAVLink_message]:
+        try:
+            return next(self.msg_queue)
+        except StopIteration:
+            return None
+
+
+def build_message(message_type, ts: Optional[datetime.datetime] = None, **kwargs):
+    ts = ts or datetime.datetime.now().timestamp()
+    kwargs.setdefault("_timestamp", ts)
+    return type(f"TestMavlinkMessage_{message_type}", (MAVLink_message,), kwargs)(
+        msgId=0, name=message_type
+    )
+
+
+@pytest.fixture()
+def mock_mavlink_stub():
+    stub = StubMavlink()
+    with mock.patch.object(
+        mavlog2csv, "mavlink_connect", return_value=contextlib.nullcontext(stub)
+    ):
+        yield stub
+
+
+@pytest.mark.parametrize(
+    ("message", "is_bad"),
+    [
+        (MAVLink_message(0, "BAD_DATA"), True),
+        (MAVLink_message(0, "GPS"), False),
+        (None, True),
+    ],
+)
+def test_is_message_bad(message, is_bad):
+    assert mavlog2csv.is_message_bad(message) == is_bad
+
+
+@pytest.mark.parametrize(
+    ("cli_column", "expected", "must_fail"),
+    [
+        ("GPS.Lng", ("GPS", "Lng"), False),
+        ("ASPD.Airspeed", ("ASPD", "Airspeed"), False),
+        ("GPS", None, True),
+        ("", None, True),
+    ],
+)
+def test_parse_cli_column(cli_column, expected, must_fail):
+    if must_fail:
+        with pytest.raises(ValueError):
+            mavlog2csv.parse_cli_column(cli_column)
+    else:
+        assert mavlog2csv.parse_cli_column(cli_column) == expected
+
+
+def test_open_output_file(tmp_path):
+    file_path = tmp_path / "test.bin"
+
+    ctx = mavlog2csv.open_output(file_path)
+    assert hasattr(ctx, "__enter__")
+    with ctx as enter_result:
+        assert isinstance(enter_result, IOBase)
+
+
+def test_open_output_no_file():
+    ctx = mavlog2csv.open_output()
+    assert hasattr(ctx, "__enter__")
+    with ctx as enter_result:
+        assert isinstance(enter_result, IOBase)
+
+
+def test_iter_mavlink_messages(mock_mavlink_stub):
+    mock_mavlink_stub.messages = [MAVLink_message(0, name="TEST")]
+    mock_mavlink_stub.start()
+
+    received_messages = list(
+        mavlog2csv.iter_mavlink_messages(device="test", types={"TEST"}, skip_n_arms=0)
+    )
+
+    assert len(received_messages) == 1
+
+
+def test_iter_mavlink_messages_skip_n_arms(mock_mavlink_stub):
+    mock_mavlink_stub.messages = [
+        build_message("TEST"),
+        build_message("EV", Id=10),
+        build_message("TEST"),
+    ]
+    mock_mavlink_stub.start()
+
+    received_messages = list(
+        mavlog2csv.iter_mavlink_messages(device="test", types={"TEST"}, skip_n_arms=1)
+    )
+
+    assert len(received_messages) == 1
+
+
+@pytest.mark.parametrize(
+    ("message", "columns", "row"),
+    [
+        (
+            build_message(
+                "TEST",
+                TimeUS=1_000_000 * 5,
+                col1="test",
+                _timestamp=datetime.datetime(2023, 1, 1, 1, 1, 1).timestamp(),
+            ),
+            ["col1"],
+            {
+                "TimeUS": 1_000_000 * 5,
+                "TimeS": 5.0,
+                "TEST.col1": "test",
+                "Date": datetime.date(2023, 1, 1).isoformat(),
+                "Time": datetime.time(1, 1, 1).isoformat(),
+            },
+        )
+    ],
+)
+def test_message_to_row(message, columns, row):
+    assert mavlog2csv.message_to_row(message, columns) == row
+
+
+def test_mavlog2csv(mock_mavlink_stub, tmp_path):
+    dt = datetime.datetime.now()
+    mock_mavlink_stub.messages = [
+        build_message("TEST", col1="ignored"),
+        build_message("EV", Id=10),
+        build_message("OTHER", col1="ignored"),
+        build_message(
+            "TEST", col1="used", TimeUS=1_000_000 * 5, _timestamp=dt.timestamp()
+        ),
+    ]
+    mock_mavlink_stub.start()
+
+    output_file = tmp_path / "output.csv"
+
+    mavlog2csv.mavlog2csv(
+        device="test",
+        output=str(output_file),
+        columns=["TEST.col1"],
+        skip_n_arms=1,
+    )
+
+    assert output_file.exists()
+    with output_file.open() as csv_file:
+        csv_reader = csv.reader(csv_file)
+        rows = list(csv_reader)
+
+    assert len(rows) == 2
+    assert rows[1] == [
+        "5000000",
+        "5.0",
+        dt.date().isoformat(),
+        dt.time().isoformat(),
+        "used",
+    ]
